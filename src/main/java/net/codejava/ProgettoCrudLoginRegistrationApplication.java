8c0efbb33a0206fb38e0463955db095f353b5fa8
Andrea Favara <andrea.favara13@gmail.com>
2022-10-31 10:06:12 +0100

    sono un commento

---
+++ src/main/java/net/codejava/ProgettoCrudLoginRegistrationApplication.java
@@ -0,0 +1,13 @@
+package net.codejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoCrudLoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoCrudLoginRegistrationApplication.class, args);
+	}
+
+}
